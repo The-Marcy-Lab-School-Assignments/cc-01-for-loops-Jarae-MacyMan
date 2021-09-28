bf3c61a6c1bb5c8716e6e7f0a2b23c4ceebc923e
Your Name <Your Github Email Address>
2021-09-28 14:43:48 +0000

    completed all questions

--- index.js
+++ index.js
@@ -1,21 +1,42 @@
 //1
 function countToFifty(){
-  
+  for (var i = 1; i <= 50; i++) {
+      console.log(i)
+  }
+ 
 }
 
 //2
-function countFromOne(){
-  
+function countFromOne(num){
+  for (var i = 1; i <= num; i++) {
+      console.log(i)
+  }
 }
 
 //3
 function threeAndFiveInFifty(){
-  
+    for (var i = 1; i <= 50; i++) {
+        if ((i % 3 == 0) ||  (i % 5 === 0)){
+    console.log(i);
+         }
+    }
 }
 
 //4
-function threeAndFive(){
-  
+function threeAndFive(num){
+    for (var i = 1; i <= num; i++) {
+        if ((i % 3 == 0) ||  (i % 5 === 0)){
+            console.log(i);
+         }
+    }
 }
 
 
+function triple (num){
+    let aRR = [];
+    for (var i = 1; i <= aRR.length ; i++) {
+        let trip = aRR[i] * 3;
+        aRR.push(trip);
+    }
+    
+}
